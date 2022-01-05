fdbc88d2308fbc1279673f4916494736baf151a7
aralon2843 <aralon2843@gmail.com>
2022-01-05 16:36:08 +0300

    connected to cloud mongoDB

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { mongoose } from '@typegoose/typegoose';
 import { AppModule } from './app.module';
+
+const mongoDBUrl =
+  'mongodb+srv://aralon2843:qwertyslav1234@cluster0.jgaku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await mongoose.connect(mongoDBUrl);
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  await app.listen(3001);
 }
 bootstrap();
